27975b7523bdc69756d05cd27ebeaffa5229e156
Kyaw Thiha <67072060+kth4dev@users.noreply.github.com>
2022-06-27 00:10:26 +0630

    final

--- check.js
+++ check.js
@@ -1,23 +1,16 @@
-function kth() {
-document.getElementById("k").innerHTML="<a>hello</a>";
-document.getElementById("q").value="work";
-}
+
 
     function getParameters() {
-
         const searchParams = new URLSearchParams(window.location.search);
-document.getElementById("q").value=searchParams.get("q");
+        var productId=searchParams.get("product");
+        var url="intent://app/MainActivity#Intent;scheme=tta://"+productId+";package=com.tta.qr;end";
+        document.getElementById("q").value=productId;
+          window.location.replace(url);
+         setTimeout(fallbackToStore, 700);
 
     }
 
 
     function fallbackToStore() {
   window.location.replace('https://play.google.com/store/apps/details?id=com.github.android');
-};
-function openApp() {
-  window.location.replace('intent://app/MainActivity#Intent;scheme=tta;package=com.tta.qr;end');
-};
-function triggerAppOpen() {
-  openApp();
-  setTimeout(fallbackToStore, 700);
 };
